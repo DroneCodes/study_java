f25a09499d8ebbddd57734d2d3b3516da43e2381
DroneCodes <oluwafisayomiogunyemi@gmail.com>
2023-10-10 01:15:30 -0400

    Walking my way around Recursion

--- src/Recursions/Main.java
+++ src/Recursions/Main.java
@@ -1,5 +1,7 @@
 package Recursions;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,16 +9,45 @@ public class Main {
         /*
             Recursion:
 
-            1. A method that calls itself.
-            2. A method that calls itself directly or indirectly.
+            1. A function that calls itself.
+            2. A function that calls itself directly or indirectly.
+
+            Example:
+
+            void message(int n) {
+                if (n == 5) {
+                    return; // go back to the previous method call
+                } // this statement gives us a break point to stop the recursion.
+                System.out.println(n);
+                message(n + 1); // this is the part of the code where recursion occurs
+            }
+
+            // Why do we need Recursion?
+
+
          */
 
         message(1);
+        printName();
     }
 
-    static  void message(int n) {
+    static void message(int n) {
+
+        if (n == 5) {
+            return;
+        }
         System.out.println(n);
         message(n + 1);
+    }
+
+    static void printName() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+
+        System.out.println("Hello " + name);
+
+        printName();
     }
 
 }
