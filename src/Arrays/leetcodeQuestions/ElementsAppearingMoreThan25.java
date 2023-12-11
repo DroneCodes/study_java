d1cc0fa81b3f2752dbb2d9eb38e3e5029749dcde
DroneCodes <oluwafisayomiogunyemi@gmail.com>
2023-12-11 09:59:21 -0500

    Solving Questions on LeetCode

---
+++ src/Arrays/leetcodeQuestions/ElementsAppearingMoreThan25.java
@@ -0,0 +1,17 @@
+package Arrays.leetcodeQuestions;
+
+public class ElementsAppearingMoreThan25 {
+    public int findSpecialInteger(int[] arr) {
+        int n = arr.length;
+        int count = 1;
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                count++;
+                if (count > n / 4)
+                    return arr[i];
+            } else
+                count = 1;
+        }
+        return arr[n - 1];
+    }
+}
