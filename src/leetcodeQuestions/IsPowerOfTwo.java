28de83072affcaf08c4eb9c7204261dc727362b3
DroneCodes <oluwafisayomiogunyemi@gmail.com>
2024-02-23 22:22:06 -0500

    Solving Questions on LeetCode

--- src/leetcodeQuestions/IsPowerOfTwo.java
+++ src/leetcodeQuestions/IsPowerOfTwo.java
@@ -7,14 +7,19 @@ public class IsPowerOfTwo {
             return false;
         }
 
+
         while(n % 2 == 0){
             n = n / 2;
         }
-
         if(n == 1){
             return true;
         }
 
         return false;
+    }
+
+    public static void main(String[] args) {
+        IsPowerOfTwo isPowerOfTwo = new IsPowerOfTwo();
+        System.out.println(isPowerOfTwo.isPowerOfTwo(15));
     }
 }
