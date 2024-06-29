ff3894599bbe860d0306acc4961db77b808270f4
DroneCodes <oluwafisayomiogunyemi@gmail.com>
2024-06-28 21:24:43 -0400

    Solving Questions on LeetCode

--- src/LinkedList/CircularLinkedList.java
+++ src/LinkedList/CircularLinkedList.java
@@ -1,5 +1,18 @@
 package LinkedList;
 
+import LinkedList.type_declaration.CircularListNode;
+
 public class CircularLinkedList {
 
+    // Counting nodes in a Circular Linked List
+
+    public int countNodes(CircularListNode head) {
+        int length = 0;
+        CircularListNode current = head.getNext();
+        while (current != head) {
+            length++;
+            current = current.getNext();
+        }
+        return length;
+    }
 }
